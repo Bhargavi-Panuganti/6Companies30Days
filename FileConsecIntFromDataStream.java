9ae26a3016c9fb52e4aa47daf85a5ebfd37e2e9b
Bhargavi <161690421+Bhargavi-Panuganti@users.noreply.github.com>
2025-01-28 19:50:42 +0530

    Create FileConsecIntFromDataStream.java

---
+++ FileConsecIntFromDataStream.java
@@ -0,0 +1,21 @@
+class DataStream {
+    private int value;
+    private int k;
+    private int count;
+    public DataStream(int value, int k) {
+        this.value=value;
+        this.k=k;
+        this.count=0;
+    }
+    
+    public boolean consec(int num) {
+        count=(value==num)?count+1:0;
+        return count>=k;
+    }
+}
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream obj = new DataStream(value, k);
+ * boolean param_1 = obj.consec(num);
+ */
